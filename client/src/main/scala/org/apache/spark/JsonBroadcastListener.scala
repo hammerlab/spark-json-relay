7d5ee400421d749625325075ad48899c5091f7dd
Ryan Williams <ryan.blake.williams@gmail.com>
2015-06-16 06:53:06 +0000

    move to sending via Sockets
    fixes #1

--- client/src/main/scala/org/apache/spark/JsonBroadcastListener.scala
+++ client/src/main/scala/org/apache/spark/JsonBroadcastListener.scala
@@ -1,33 +1,44 @@
 package org.apache.spark
 
-import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.http.{RequestBuilder, Http}
+import java.io.{Writer, OutputStreamWriter}
+import java.net.{Socket, SocketException}
+import java.nio.charset.Charset
+import javax.net.SocketFactory
+
 import org.json4s.jackson.JsonMethods._
 import org.json4s.JsonDSL._
-import com.twitter.finagle.Service
-import com.twitter.util.{Await, Future}
 import org.apache.spark.scheduler.SparkListenerEvent
 import org.apache.spark.util.JsonProtocol
-import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
 import org.json4s.JsonAST.{JObject, JNothing, JValue}
-import org.jboss.netty.buffer.ChannelBuffers.wrappedBuffer
 
 class JsonBroadcastListener(conf: SparkConf) extends SparkFirehoseListener {
 
   val appId = conf.get("spark.app.id")
   val host = conf.get("spear.host", "localhost")
   val port = conf.getInt("spear.port", 8123)
-  val retries = conf.getInt("spear.retries", 2)
-  val connectionLimit = conf.getInt("spear.connection-limit", 1)
+  val debugLogs = conf.getBoolean("spear.verbose", false)
 
-  val client: Service[HttpRequest, HttpResponse] =
-    ClientBuilder()
-      .codec(Http())
-      .hosts(s"$host:$port")
-      .hostConnectionLimit(connectionLimit)
-      .retries(retries)
-      .name("json-client")
-      .build()
+  val socketFactory = SocketFactory.getDefault
+  var socket: Socket = _
+  var writer: Writer = _
+  val utf8 = Charset.forName("UTF-8")
+
+  var numReqs = 0
+  var lastEvent: Option[String] = None
+
+  def print(s: String): Unit = {
+    if (debugLogs) {
+      println(s)
+    }
+  }
+
+  def initSocketAndWriter() = {
+    print("*** Initializing socket ***")
+    socket = socketFactory.createSocket(host, port)
+    writer = new OutputStreamWriter(socket.getOutputStream, utf8)
+  }
+
+  initSocketAndWriter()
 
   override def onEvent(event: SparkListenerEvent): Unit = {
     val jv: JValue = JsonProtocol.sparkEventToJson(event) match {
@@ -36,18 +47,28 @@ class JsonBroadcastListener(conf: SparkConf) extends SparkFirehoseListener {
       case j => throw new Exception(s"Non-object SparkListenerEvent $j")
     }
     val s: String = compact(jv)
-    val payload = s.getBytes("UTF-8")
-    val request =
-      RequestBuilder().url(s"http://$host:$port/")
-        .setHeader("Content-Type", "text/json")
-        .setHeader("Content-Length", payload.length.toString)
-        .buildPost(wrappedBuffer(payload))
 
-    val response: Future[HttpResponse] = client(request)
-    response onSuccess { resp: HttpResponse =>
-    } onFailure { t: Throwable =>
-      throw new Exception("Request failed", t)
+    numReqs += 1
+    print(s"Socket To send request: $numReqs\n$s\n")
+    try {
+      if (socket.isClosed) {
+        print(s"*** Socket is closed... ***")
+      }
+      writer.write(s)
+      writer.flush()
+    } catch {
+      case e: SocketException =>
+        socket.close()
+        initSocketAndWriter()
+        print(s"Socket re-sending: $lastEvent and $s")
+        lastEvent.foreach(writer.write)
+        writer.write(s)
+        writer.flush()
+        print("Socket re-sent")
     }
-    Await.ready(response)
+
+    lastEvent = Some(s)
+
+    print(s"Socket Sending request: $numReqs")
   }
 }
